1eb1e85d2fbe35a39ffca6e45f1bf792a1334889
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2023-05-01 15:08:16 -0600

    Completed Project

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,35 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: null
+    }
+  };
+
+  handleChange = (e) => {
+    this.setState({...this.state, task: e.target.value});
+  }
+
+  handleAddTask = (e) => {
+    e.preventDefault();
+    this.props.addTask(this.state.task);
+    document.getElementById("newTask").value = "";
+  }
+
+  handleRemoveTasks = (e) => {
+    e.preventDefault();
+    this.props.removeTasks();
+  }
+
   render() {
     return (
-      <div>
-        Form
-      </div>
+      <form className="form">
+        <input onChange={this.handleChange} type="text" id="newTask" name="newTask" placeholder="Enter New Tasks Here" />
+        <button onClick={this.handleAddTask}>Add Task</button>
+        <button onClick={this.handleRemoveTasks}>Remove Completed Tasks</button>
+      </form>
     )
-  }
+  };
 }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 export default class Todo extends React.Component {
+  handleClick = () => {
+    this.props.toggleDone(this.props.todo.id);
+  }
+
   render() {
     return (
-      <div>
-        Todo
-      </div>
+      <li onClick={this.handleClick} className={this.props.todo.completed ? 'complete' : ''}>{this.props.todo.task}</li>
     )
-  }
+  };
 }
